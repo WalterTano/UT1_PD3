d1dbe5f0735affd4422f08658964c6ae11733c72
Walter Taño <tano.walter@hotmail.com>
2022-03-26 16:04:59 -0300

    Paso 3

---
+++ UT1_PD/src/ut1_pd3/Mamifero.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package ut1_pd3;
+
+/**
+ *
+ * @author TanoW
+ */
+public interface Mamifero {
+    
+    public String getNombre();
+    public boolean respondeA(String unNombre);
+    
+    public void caminar();
+    public void correr();
+    public void saltar();
+}
